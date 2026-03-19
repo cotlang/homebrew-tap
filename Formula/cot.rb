fae17503774d4827763b8147584e860755d66124
John Cottrell <john@cot.dev>
2026-03-19 14:52:06 +1100

    Update cot to 0.3.7

--- Formula/cot.rb
+++ Formula/cot.rb
@@ -1,20 +1,20 @@
 class Cot < Formula
   desc "Write like TypeScript, run like Rust, deploy anywhere"
   homepage "https://github.com/cotlang/cot"
-  version "0.3.6"
+  version "0.3.7"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/cotlang/cot/releases/download/v0.3.6/cot-aarch64-macos.tar.gz"
-      sha256 "e49b52c69f98c8663fc8a27452d4602832dde40575c9a220cd05b0aeda327601"
+      url "https://github.com/cotlang/cot/releases/download/v0.3.7/cot-aarch64-macos.tar.gz"
+      sha256 "5f30980804c12c73250fce86a27ace6757c7ddb2256c8a20fed8dd38419ad981"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/cotlang/cot/releases/download/v0.3.6/cot-x86_64-linux.tar.gz"
-      sha256 "36fc561bd2d9aaf9f1bb92afa6ada2b2646e037c991f80057bb2eab2e29aa1c4"
+      url "https://github.com/cotlang/cot/releases/download/v0.3.7/cot-x86_64-linux.tar.gz"
+      sha256 "83385eefadaeaafe7c0445125e49886604052afe3eaf19eb387f8008f869e9a2"
     end
   end
 
